91ebf2fa2a9796f3fce431b450f1697a4a000852
Alexis Inthanavong <ainthana@bess-f4r4s7.clusters.42paris.fr>
2024-11-26 17:57:27 +0100

    changegement exos maj

--- Makefile
+++ Makefile
@@ -10,7 +10,12 @@ SRC = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c \
 						ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c \
 							ft_substr.c ft_strjoin.c ft_striteri.c ft_strmapi.c ft_itoa.c ft_strtrim.c ft_split.c
 
+SRC_BONUS = ft_lstnew_bonus.c ft_lstadd_front_bonus.c ft_lstsize_bonus.c \
+				ft_lstlast_bonus.c ft_lstadd_back_bonus.c ft_lstdelone_bonus.c ft_lstclear_bonus.c ft_lstiter_bonus.c ft_lstmap_bonus.c
+
 OBJ = $(SRC:.c=.o)
+
+BONUS_OBJ = $(SRC_BONUS:.c=.o)
 
 all : $(NAME)
 
@@ -20,12 +25,15 @@ $(NAME) : $(OBJ)
 .c.o:
 	$(CC) $(CFLAGS) -c $< -o $@
 
+bonus: $(NAME) $(BONUS_OBJ)
+	ar -rsc $(NAME) $(BONUS_OBJ)
+
 clean :
-		rm -f $(OBJ)
+		rm -f $(OBJ) $(BONUS_OBJ)
 
 fclean : clean
 		rm -f $(NAME)
 
 re : fclean all
 
-.PHONY : clean all fclean re
+.PHONY : clean all fclean re bonus
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ainthana <ainthana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 11:04:44 by ainthana          #+#    #+#             */
-/*   Updated: 2024/11/15 15:25:01 by ainthana         ###   ########.fr       */
+/*   Updated: 2024/11/26 17:43:05 by ainthana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,4 +57,20 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-#endif	
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+#endif
